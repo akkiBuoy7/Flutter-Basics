e14b4dd5cead09238e90e172f63552e055abaa95
akkiB <akash3350saha@gmail.com>
2023-03-09 00:01:22 +0530

    Text widget Implementation

--- lib/main.dart
+++ lib/main.dart
@@ -22,31 +22,37 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}):super(key:key);
+  const MyHomePage({Key? key}) : super(key: key);
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text("Flutter Container"),
       ),
-      body:Align(
+      body: Align(
         alignment: Alignment.center,
         child: Container(
-          width: 100,
-          height: 100,
+          width: 300,
+          height: 300,
           color: Colors.blue,
-          child: Center(child: Text("Hello World",style: TextStyle(color: Colors.white))),
+          child: Center(
+              child: Text("Hello World",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.w700 // bold or thin
+                    fontFamily:
+                  ))),
         ),
-      ) ,
+      ),
     );
   }
 }

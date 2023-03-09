2598efd56f418b0c2d7d0c779e8a92cc21b4330b
akkiB <akash3350saha@gmail.com>
2023-03-09 09:41:09 +0530

    Nested row column

--- lib/RowColumn.dart
+++ lib/RowColumn.dart
@@ -38,6 +38,16 @@ MainAxisAlignment -> can change alignment left or right
  spaceBetween -> no padding
  */
 
+/*
+Column arranges vertically
+main_axis -> vertical
+cross axis -> Horizontal
+MainAxisAlignment -> can change alignment left or right
+ spaceAround -> padding at both ends are equal
+ spaceEvenly -> equally divided across width or length of axis
+ spaceBetween -> no padding
+ */
+
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
@@ -47,18 +57,73 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text("Flutter Container"),
       ),
-      body: Container(
-        color: Colors.lightGreen,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text("Some text 1"),
-            Text("Some Text 2"),
-            TextButton(onPressed: (){}, child: Text("Click here!")),
-            ElevatedButton(onPressed: (){}, child: Text("Click Here!"))
-          ],
-
-        ),
+      body: Column(
+        children: [
+          Container(
+            color: Colors.lightGreen,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Some text 1"),
+                Text("Some Text 2"),
+                TextButton(onPressed: () {}, child: Text("Click here!")),
+                ElevatedButton(onPressed: () {}, child: Text("Click Here!"))
+              ],
+            ),
+          ),
+          Container(
+            color: Colors.yellow,
+            height: 300,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  "Some text 1",
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  "Some Text 2",
+                  textAlign: TextAlign.center,
+                ),
+                TextButton(onPressed: () {}, child: Text("Click here!")),
+                ElevatedButton(onPressed: () {}, child: Text("Click Here!")),
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text("Again text 1"),
+                      Text("Again Text 2"),
+                      TextButton(
+                          onPressed: () {}, child: Text("Again Click here!")),
+                      ElevatedButton(
+                          onPressed: () {}, child: Text("Click Here!")),
+                      Expanded(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Text(
+                              "Some text 1",
+                              textAlign: TextAlign.center,
+                            ),
+                            Text(
+                              "Some Text 2",
+                              textAlign: TextAlign.center,
+                            ),
+                            Text(
+                              "Some Text 3",
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }

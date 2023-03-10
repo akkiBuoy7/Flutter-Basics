10ee44d8395519bc3905ab26c895d364470e560e
akkiB <akash3350saha@gmail.com>
2023-03-10 09:11:07 +0530

    Date and time picker. Need to solve future for time picker

--- lib/DateTime.dart
+++ lib/DateTime.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_basics/ui_helper/util.dart';
 import 'package:intl/intl.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -30,8 +31,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   var time = DateTime.now();
+  var format = "";
 
   @override
   Widget build(BuildContext context) {
@@ -45,13 +46,51 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("CurrentTime:${DateFormat('Hms').format(time)}",style: customTextstyle(),),
-            SizedBox(height: 10,),
-            ElevatedButton(onPressed: (){
-              setState(() {
+            Text(
+              "CurrentTime:${DateFormat(format).format(time)}",
+              style: customTextstyle(),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  format = 'Hms';
+                  setState(() {});
+                },
+                child: Text("Get Time")),
+            SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+                onPressed: () async {
+                  DateTime? datePicked = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2022),
+                      lastDate: DateTime(2024));
 
-              });
-            }, child: Text("Get Time"))
+                  if (datePicked != null) {
+                    print("${datePicked.day}");
+                  }
+                },
+                child: Text("Date Picker")),
+            SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  Future<TimeOfDay?> pickedTime = showTimePicker(context: context,
+                      initialTime: TimeOfDay.now(),
+                      initialEntryMode: TimePickerEntryMode.input);
+
+                  if(pickedTime!=null){
+                    int? v = 0;
+                    pickedTime.then((value) => v=value?.hour);
+                    print("************$v");
+                  }
+                },
+                child: Text("Time Picker")),
           ],
         ),
       ),
@@ -59,5 +98,5 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 /*
-need to use intl library in pubs file to use DateFormat class
+need to use intl library in pubs file to use DateFormat class""
  */

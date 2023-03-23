e80d9fdc391b153ec53166e261f705138727812b
akkiB <akash3350saha@gmail.com>
2023-03-23 16:13:59 +0530

    drawer

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -29,22 +29,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text("Flutter Container"),
       ),
-      body:  Column(
-          children: [
-            Container(color: Colors.redAccent, width: 100, height: 100,)
-          ]
-      ),
-      drawer: Drawer(
+      endDrawer: Drawer(
         child: ListView(
-          children: const <Widget>[
+          children: <Widget>[
             DrawerHeader(
               decoration: BoxDecoration(
                 color: Colors.purple,
@@ -60,6 +58,54 @@ class _MyHomePageState extends State<MyHomePage> {
             ListTile(
               title: Text("My Account"),
               leading: Icon(Icons.account_box),
+              onTap: () {
+                print("right drawer");
+                toggleRightDrawer();
+              },
+            ),
+            Divider(
+              height: 0.2,
+            ),
+            ListTile(
+              title: Text("My Orders"),
+            ),
+            ListTile(
+              title: Text("About Us"),
+            ),
+            ListTile(
+              title: Text("Contact Us"),
+            ),
+          ],
+        ),
+      ),
+      body: Column(children: [
+        Container(
+          color: Colors.redAccent,
+          width: 100,
+          height: 100,
+        )
+      ]),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.purple,
+              ),
+              child: Text(
+                'My First App',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text("My Account"),
+              leading: Icon(Icons.account_box),
+              onTap: () {
+                toggleLeftDrawer();
+              },
             ),
             Divider(
               height: 0.2,
@@ -78,5 +124,23 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       backgroundColor: Colors.grey[100],
     );
+  }
+
+  // for left drawer
+  toggleLeftDrawer() async {
+    if (_scaffoldKey.currentState?.isDrawerOpen == true) {
+      _scaffoldKey.currentState?.openEndDrawer();
+    } else {
+      _scaffoldKey.currentState?.openDrawer();
+    }
+  }
+
+  // for right drawer
+  toggleRightDrawer() async {
+    if (_scaffoldKey.currentState!.isEndDrawerOpen) {
+      _scaffoldKey.currentState!.openDrawer();
+    } else {
+      _scaffoldKey.currentState!.openEndDrawer();
+    }
   }
 }
